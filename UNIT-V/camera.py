4e86a18a00e25089f363294e145f24f494ed5b4d
kymybk <kymybk.dev@gmail.com>
2020-08-10 23:54:39 +0900

    カメラから圧縮した画像データを送信するように修正

--- UNIT-V/camera.py
+++ UNIT-V/camera.py
@@ -1,16 +1,12 @@
-# Untitled - By: y-kuboyama - 水 7月 29 2020
-
-
 from machine import UART
 from board import board_info
 from fpioa_manager import fm
 from Maix import GPIO
-import sensor, lcd, ubinascii
+import sensor, lcd
 
 sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)#QVGA=320x240
-#sensor.set_windowing((64,160))
 sensor.set_vflip(1)
 sensor.set_hmirror(1)
 sensor.run(1)
@@ -25,6 +21,11 @@ uart = UART(UART.UART1, 1152000,8,0,0, timeout=1000, read_buf_len=4096)
 
 while(True):
     img = sensor.snapshot()
-    #print(str(ubinascii.b2a_base64(img.compress(60)))) // compress and send it over serial in base64
-    # compress and send it over serial in base64
-    uart.write(str(ubinascii.b2a_base64(img.compress(60))))
+    img_buf = img.compress(quality=50)
+    img_size1 = (img.size()& 0xFF0000)>>16
+    img_size2 = (img.size()& 0x00FF00)>>8
+    img_size3 = (img.size()& 0x0000FF)>>0
+    data_packet = bytearray([0xFF,0xD8,0xEA,0x01,img_size1,img_size2,img_size3,0x00,0x00,0x00])
+
+    uart.write(data_packet)
+    uart.write(img_buf)
